a92fe14153d0cb5eef08f8a5de8b845bad3415ed
kam6y <daiyakanba0305@gmail.com>
2026-02-19 12:22:51 +0900

    feat: SpeechRecognizerプラグインをCocoaPodsで統合し、関連するXcode設定とAppDelegateを更新

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -12,6 +12,6 @@ import UIKit
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
-    SpeechRecognizerPlugin.register(with: engineBridge.pluginRegistry.registrar(forPlugin: "SpeechRecognizerPlugin"))
+    SpeechRecognizerPlugin.register(with: engineBridge.pluginRegistry.registrar(forPlugin: "SpeechRecognizerPlugin")!)
   }
 }
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/goodapple/flutter"
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.32.4/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/goodapple/workspace/Speech to text"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
