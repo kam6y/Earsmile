c3c412f88bd76a9339b97a9b7d2cd83d0204fd76
kam6y <daiyakanba0305@gmail.com>
2026-02-19 14:58:48 +0900

    feat: 日本語ロケールの日付書式設定を初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -16,6 +17,7 @@ void main() async {
 
   final localStorageService = LocalStorageService();
   await localStorageService.initialize();
+  await initializeDateFormatting('ja');
 
   final speechService = SpeechService();
 
